c1c73de3758248993dbe247a345b9e6149439738
Moudhaffer <moudhaffer.bouallegui@esprit.tn>
2024-05-24 15:58:45 +0200

    data ingestion logic added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Text-Summarizer
+# End to end Text-Summarizer
+
+## Workflows
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update configuration manager in src config
+5. Update components
+6. Update the pipeline
+7. Update main.py
+8. Update app.py
---
+++ main.py
@@ -0,0 +1,13 @@
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.logging import logger
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
